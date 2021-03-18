0371246713c6b71309f4cae3404608ff623c4cd2
Nalcant <73491211+Nalcant@users.noreply.github.com>
2021-03-18 17:48:01 -0300

    SliderAtt

---
+++ RPG/Assets/Script/MenuSlider.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MenuSlider : MonoBehaviour
+{
+   public bool menuIsShowing = false;
+   public void MenuSliderController()
+    {
+        if(menuIsShowing == false)
+        {
+            SlideRight();
+        }
+        else if (menuIsShowing==true)
+        {
+            SlideLeft();
+        }
+    }
+    public void SlideRight()
+    {
+      
+        transform.LeanMoveLocal(new Vector2((float)-295.1, 1), 1).setEaseOutQuart();
+        menuIsShowing = true;
+
+    }
+    private void SlideLeft()
+    {
+        transform.LeanMoveLocal(new Vector2((float)-728.9001, 1), 1).setEaseOutQuart();
+        menuIsShowing = false;
+    }
+}
+

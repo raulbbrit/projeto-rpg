ce6792f964bf60c71dc4395ee78d60283ba568cb
Nalcant <73491211+Nalcant@users.noreply.github.com>
2021-04-08 15:13:36 -0300

    Tabuleiro e camera
    Fim da base do layout, inicio da construção do tabuleiro e movimento de câmera

---
+++ RPG/Assets/Script/CameraMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private float cameraSpeed = 0;
+    [SerializeField] private float xMax;
+    [SerializeField] private float yMin;
+    [SerializeField] private Vector2 gameLimit;
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        GetImput();
+    }
+    private void GetImput()   
+    {
+        Vector3 pos = transform.position;
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up* cameraSpeed * Time.deltaTime);//deltatime: espaço de tempo do ultimo update
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * cameraSpeed * Time.deltaTime);//deltatime: espaço de tempo do ultimo update
+
+        }
+      
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * cameraSpeed * Time.deltaTime);//deltatime: espaço de tempo do ultimo update
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * cameraSpeed * Time.deltaTime);//deltatime: espaço de tempo do ultimo update
+
+        }
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, 1, xMax), Mathf.Clamp(transform.position.x, yMin, 0),0);
+       
+    }
+
+    public void SetLimits(Vector3 maxTile)
+    {
+        Vector3 wp = Camera.main.ViewportToWorldPoint(new Vector3(1, 0));
+        xMax = maxTile.x - wp.x;
+        yMin = maxTile.y - wp.y;
+    }
+}
---
+++ RPG/Assets/Script/Point.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Point 
+{
+    private float x;
+    private float y;
+
+    public Point(float x, float y) : this()
+    {
+        X = x;
+        Y = y;
+    }
+
+    public float X { get => x; set => x = value; }
+    public float Y { get => y; set => y = value; }
+
+}

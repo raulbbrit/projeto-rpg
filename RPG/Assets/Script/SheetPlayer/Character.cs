911f3e03a42e0916e7212d68acc5757d8687f415
Raul <raulbeserrabrito@gmail.com>
2021-05-13 17:14:55 -0300

    Merge mod-ficha

--- RPG/Assets/Script/SheetPlayer/Character.cs
+++ RPG/Assets/Script/SheetPlayer/Character.cs
@@ -2,23 +2,85 @@
 
 public class Character : MonoBehaviour
 {
+    [Header("Character Stats")]
     public CharacterStat Strenght;
     public CharacterStat Agility;
     public CharacterStat Intelligence;
     public CharacterStat Vitality;
 
+    [Space]
+    [Header("Charecter Skills")]
+    public CharacterSkill Fight;
+    public CharacterSkill Shoot;
+    public CharacterSkill Brawl;
+    public CharacterSkill Dodge;
+    public CharacterSkill Block;
+    public CharacterSkill Athletics;
+    public CharacterSkill Physique;
+    public CharacterSkill Sneak;
+    public CharacterSkill Investigate;
+    public CharacterSkill Perception;
+    public CharacterSkill Language;
+    public CharacterSkill Knowledge;
+    public CharacterSkill Resources;
+    public CharacterSkill Intimidation;
+    public CharacterSkill Deceive;
+
+    [Space]
+    [Header("Character Information")]
+    public CharacterInfos Name;
+    public CharacterInfos Level;
+    public CharacterInfos Health;
+    public CharacterInfos Mana;
+
+    
     [SerializeField] Inventory inventory;
     [SerializeField] EquipmentPanel equipmentPanel;
     [SerializeField] StatPanel statPanel;
+    [SerializeField] SkillPanel skillPanel;
+    [SerializeField] InfoPanel infoPanel;
+    /*
+    private Inventory inventory;
+    private EquipmentPanel equipmentPanel;
+    private StatPanel statPanel;
+    private SkillPanel skillPanel;
+    */
 
-    private void Awake()
+
+    public Inventory Inventory { get => inventory; set => inventory = value; }
+    public EquipmentPanel EquipmentPanel { get => equipmentPanel; set => equipmentPanel = value; }
+    public StatPanel StatPanel { get => statPanel; set => statPanel = value; }
+    public SkillPanel SkillPanel { get => skillPanel; set => skillPanel = value; }
+    public InfoPanel InfoPanel { get => infoPanel; set => infoPanel = value; }
+
+    /*private void Awake()
     {
         statPanel.SetStats(Strenght, Agility, Intelligence, Vitality);
         statPanel.UpadeStatValues();
 
+        skillPanel.SetSkills(Fight, Shoot, Brawl, Dodge, Block, Athletics, Physique, Sneak, Investigate, Perception, Language,
+            Knowledge, Resources, Intimidation, Deceive);
+        skillPanel.UpadeStatValues();
+        
+        inventory.OnItemRightClickedEvent += EquipFromInventory;
+        equipmentPanel.OnItemRightClickedEvent += UnequipFromEquipPanel;
+    }*/
+
+    public void SetFieldsAndUI()
+    {
+        statPanel.SetStats(Strenght, Agility, Intelligence, Vitality);
+        statPanel.UpadeStatValues();
+
+        skillPanel.SetSkills(Fight, Shoot, Brawl, Dodge, Block, Athletics, Physique, Sneak, Investigate, Perception, Language,
+            Knowledge, Resources, Intimidation, Deceive);
+        skillPanel.UpadeStatValues();
+
+        infoPanel.SetInfos(Name, Level, Health, Mana);
+
         inventory.OnItemRightClickedEvent += EquipFromInventory;
         equipmentPanel.OnItemRightClickedEvent += UnequipFromEquipPanel;
     }
+
 
     private void EquipFromInventory(Item item)
     {

eca411824c36b5e62846f858a3c750e8f1e56461
Nalcant <73491211+Nalcant@users.noreply.github.com>
2021-06-08 20:08:31 -0300

    Revert "att"
    This reverts commit 8094d50e97f3d7262b5509cd419e0d73e420a73f.

--- RPG/Assets/Script/Network/CharacterSpawn.cs
+++ RPG/Assets/Script/Network/CharacterSpawn.cs
@@ -6,17 +6,20 @@ using Mirror;
 public class CharacterSpawn : NetworkBehaviour
 {
     [SerializeField] private GameObject characterPreFab;
+    private NetworkPlayer networkPlayer;
 
     
     [Command]
     public void CmdSpawn()
     {
-        
+        networkPlayer = gameObject.GetComponent<NetworkPlayer>();
         Debug.Log("Entrou no método spawn");
         var valueIncrement = GameObject.Find("IncrementManager").GetComponent<ValuesIncrement>();
         characterPreFab = valueIncrement.CreateCharacter(characterPreFab).gameObject;
-        GameObject CharacterInstance = Instantiate(characterPreFab);
-        CharacterInstance.name = transform.name + " Character's";
-        NetworkServer.Spawn(CharacterInstance, connectionToClient);
+        //  GameObject CharacterInstance = Instantiate(characterPreFab);
+        //  CharacterInstance.name = transform.name + " Character's";
+        networkPlayer.Character = Instantiate(characterPreFab);
+        networkPlayer.ChangePlayerCharacterName();
+        NetworkServer.Spawn(networkPlayer.Character, connectionToClient);
     }
 }

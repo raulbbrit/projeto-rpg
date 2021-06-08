8094d50e97f3d7262b5509cd419e0d73e420a73f
Nalcant <73491211+Nalcant@users.noreply.github.com>
2021-06-08 17:15:37 -0300

    att

--- RPG/Assets/Script/Network/CharacterSpawn.cs
+++ RPG/Assets/Script/Network/CharacterSpawn.cs
@@ -6,20 +6,17 @@ using Mirror;
 public class CharacterSpawn : NetworkBehaviour
 {
     [SerializeField] private GameObject characterPreFab;
-    private NetworkPlayer networkPlayer;
 
     
     [Command]
     public void CmdSpawn()
     {
-        networkPlayer = gameObject.GetComponent<NetworkPlayer>();
+        
         Debug.Log("Entrou no método spawn");
         var valueIncrement = GameObject.Find("IncrementManager").GetComponent<ValuesIncrement>();
         characterPreFab = valueIncrement.CreateCharacter(characterPreFab).gameObject;
-        //  GameObject CharacterInstance = Instantiate(characterPreFab);
-        //  CharacterInstance.name = transform.name + " Character's";
-        networkPlayer.Character = Instantiate(characterPreFab);
-        networkPlayer.ChangePlayerCharacterName();
-        NetworkServer.Spawn(networkPlayer.Character, connectionToClient);
+        GameObject CharacterInstance = Instantiate(characterPreFab);
+        CharacterInstance.name = transform.name + " Character's";
+        NetworkServer.Spawn(CharacterInstance, connectionToClient);
     }
 }

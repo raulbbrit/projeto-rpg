a7bf1d13f817326ac8353aa1914f44a4d32e82e9
Raul <73473539+raulbbrit@users.noreply.github.com>
2021-06-14 15:13:23 -0300

    att final definitiva - Prefab-tile

--- RPG/Assets/Script/Gameplay/GameManager.cs
+++ RPG/Assets/Script/Gameplay/GameManager.cs
@@ -25,7 +25,7 @@ public class GameManager : Singleton<GameManager>
         if (NetworkClient.localPlayer.isServer)
         {
             
-            if (Hover.indexMaster < spaceTile)
+            if (Hover.indexMaster <= spaceTile)
             {
 
                 this.SelectedObject = pickObjectButton;
@@ -35,7 +35,7 @@ public class GameManager : Singleton<GameManager>
 
         else if (NetworkClient.localPlayer.isClientOnly)
             {
-                if (Hover.indexPlayer < 1)
+                if (Hover.indexPlayer <= 1)
                 {
                     Debug.Log(Hover.indexPlayer);
                     this.SelectedObject = pickObjectButton;
@@ -74,7 +74,7 @@ public class GameManager : Singleton<GameManager>
             {
                 if (NetworkClient.localPlayer.isClientOnly)
                 {
-                    if(Hover.indexPlayer == 1)
+                    if(Hover.indexPlayer >= 1)
                     {
                         if(Hover.hoverBool)
                         {
